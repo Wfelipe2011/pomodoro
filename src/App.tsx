149e82d3ac3cb62d7d9b0024dbd9a614efc42b12
Wilson Felipe <wfelipe2011@gmail.com>
2021-07-12 11:00:03 -0300

    ajust2 pwa

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,25 @@ import { Nav } from './components/Nav';
 import {  ContentProvider } from './contexts/Content';
 
 function App() {
+  if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+      console.log(
+        "[PWA Builder] active service worker found, no need to register"
+      );
+    } else {
+      // Register the service worker
+      navigator.serviceWorker
+        .register("pwabuilder-sw.js", {
+          scope: "../public/pwabuilder-sw.js",
+        })
+        .then(function (reg) {
+          console.log(
+            "[PWA Builder] Service worker has been registered for scope: " +
+              reg.scope
+          );
+        });
+    }
+  }
   return (
     <div className={`themeDark && dark`}>
       <ContentProvider>
